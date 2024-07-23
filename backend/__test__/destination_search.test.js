6aae86e7b83adb5b874e2f464e84f4dc21c517ef
ck <65394936+cherylktt@users.noreply.github.com>
2024-07-23 16:40:05 +0800

    Modified destination search backend unit and integration tests

--- backend/__test__/destination_search.test.js
+++ backend/__test__/destination_search.test.js
@@ -1,18 +1,33 @@
+const destinationSearch = require("../routes/destination_search")
+const getDestinationId = destinationSearch.getDestinationId
 const destinationText = "Istana, Singapore";
 const expectedId = "RsBU";
 
 describe("Backend Destination Search Unit Test", () => {
-    test("BACKEND_DEST_SEARCH_1: Testing destination search with valid destination text", async() => {
-        const res = await fetch(`http://localhost:5000/destination_search/${destinationText}`, {
+    test("BACKEND_DEST_SEARCH_UNIT_1: Test retrieving destination id with valid destination text", async() => {
+        const id = getDestinationId(destinationText)
+        expect(id).toBe(expectedId)
+    }, 10000)
+
+    test("BACKEND_DEST_SEARCH_UNIT_2: Test retrieving destination id with invalid destination text", async() => {
+        const invalid_destinationText = "Singapore";
+        const id = getDestinationId(destinationText)
+        expect(id).toBe(null)
+    }, 10000)
+})
+
+describe("Backend Destination Search Integration Test", () => {
+    test("BACKEND_DEST_SEARCH_INT_1: Test destination search route with valid destination text", async() => {
+        const res = await fetch(`http://localhost:5000/destination_search/`, {
             headers: {
                 "Content-type": "application/x-www-form-urlencoded"
             }
         })
-        const text = await res.text()
-        expect(text).toBe(expectedId);
+        const text = await res.text();
+        expect(text).toBe(expectedId)
     }, 10000)
 
-    test("BACKEND_DEST_SEARCH_2: Testing destination search with invalid destination text", async() => {
+    test("BACKEND_DEST_SEARCH_INT_2: Test retrieving destination id with invalid destination text", async() => {
         const invalid_destinationText = "Singapore";
         const res = await fetch(`http://localhost:5000/destination_search/${invalid_destinationText}`, {
             headers: {
@@ -26,7 +41,7 @@ describe("Backend Destination Search Unit Test", () => {
         expect(json.message).toBe("Invalid Input: Destination name is invalid.")
     }, 10000)
 
-    test("BACKEND_DEST_SEARCH_3: Testing /all in destination search", async() => {
+    test("BACKEND_DEST_SEARCH_INT_3: Testing /all in destination search", async() => {
         const res = await fetch(`http://localhost:5000/destination_search/`, {
             headers: {
                 "Content-type": "application/x-www-form-urlencoded"
@@ -34,7 +49,6 @@ describe("Backend Destination Search Unit Test", () => {
         })
         const text = await res.text();
         const json = JSON.parse(text);
-        console.log(json.length);
         expect(json.length).toBe(74280);
     }, 10000)
 })
--- backend/routes/destination_search.js
+++ backend/routes/destination_search.js
@@ -1,20 +1,22 @@
 const express = require("express");
 var router = express.Router();
-const destinationJSON = require("../destinations.json");
+const destinationJSON = require("../destinations.json")
 
-/* GET destination ID from name */
-router.get("/:destination", async function (req, res, next) {
-    const destinationName = req.params.destination
-    console.log(destinationName)
+async function getDestinationId(destName) {
     var destinationId = null
-    
-    for (let getDestination of destinationJSON) {
-        if (destinationName.includes(getDestination.term)) {
-            destinationId = getDestination.uid
-            console.log(destinationId)
+    for (let dest of destinationJSON) {
+        if (destName.includes(dest.term)) {
+            destinationId = dest.uid
             break
         }
     }
+    return destinationId
+}
+
+/* GET destination ID from name */
+router.get("/:destination", async function (req, res, next) {
+    const destName = req.params.destination
+    const destinationId = getDestinationId(destName)
 
     if (destinationId != null) {
         res.set("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -22,7 +24,7 @@ router.get("/:destination", async function (req, res, next) {
     } else {
         return res.status(400).json({message: "Invalid Input: Destination name is invalid."})
     }
-});
+})
 
 router.get("/", async function (req, res, next) {
     res.set("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -31,4 +33,7 @@ router.get("/", async function (req, res, next) {
 
 
 
-module.exports = router;
+module.exports = {
+    getDestinationId: getDestinationId, 
+    router: router
+}
