73b839d23b1d197ad7dd159b870f35ed26eae114
Edgarrighthere <edgaraw29@gmail.com>
2024-08-05 11:26:27 +0800

    latest merge with edgar2

--- backend/__test__/login.test.js
+++ backend/__test__/login.test.js
@@ -176,24 +176,24 @@ describe("Backend login integration tests", () => {
         expect(json.message).toBe("Invalid email.")
     });
 
-    test ("BACKEND_LOGIN_5: Testing invalid input", async () => {
-        //Invalid input, should have been our test user credentials
-        const payload = null;
+    // test ("BACKEND_LOGIN_5: Testing invalid input", async () => {
+    //     //Invalid input, should have been our test user credentials
+    //     const payload = null;
 
-        // //Get mock response
-        // const res = await request(app)
-        //     .post('/login')
-        //     .send(payload)
-        //     .set('Content-Type', 'application/json')
-        //     .set('Accept', 'application/json');
+    //     //Get mock response
+    //     const res = await request(app)
+    //         .post('/login')
+    //         .send(payload)
+    //         .set('Content-Type', 'application/json')
+    //         .set('Accept', 'application/json');
         
-        // const json = JSON.parse(res.text);
+    //     const json = JSON.parse(res.text);
 
-        // // console.log(json);
+    //     // console.log(json);
 
-        // expect(res.statusCode).toEqual(500);
-        // expect(json.message).toBe("An error occurred. Please try again.")
-    });
+    //     expect(res.statusCode).toEqual(500);
+    //     expect(json.message).toBe("An error occurred. Please try again.")
+    // });
     
     afterAll(async () => {
         await teardown();
